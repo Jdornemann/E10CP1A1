1c513a1ff5f781df0fe5c261049fb23701c274ff
jdornemann <johann.dornemann.c@gmail.com>
2018-11-06 21:35:47 -0300

    OK

--- ejercicio2.rb
+++ ejercicio2.rb
@@ -4,18 +4,20 @@
 # La última instrucción debe imprimir "Hola! Soy la clase MiClase"
 
 class MiClase
-    attr_writer :name
+    attr_accessor :name
     def initialize(name)
         @name = name
     end
 
     def self.saludar
          
-        "Hola! Soy la clase #{@name}"
+        "Hola! Soy la clase #{self.name}"
     end
 end
 
-c = MiClase.new('MiClase').saludar
+c = MiClase.new('MiClase')
 
-puts c
+puts MiClase.saludar 
 
+
+#Conclusión: Un metodo de clase no puede llamar a una variable de instancia
